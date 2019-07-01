1f48749c3bb3936ef4fe93c7d0bbaabf9e53961a
Your Name <you@example.com>
2019-07-01 09:24:39 +0800

    第一次添加

---
+++ src/main/java/ll.java
@@ -0,0 +1,33 @@
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ll {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+
+        Integer[] array = list.toArray(new Integer[list.size()]);
+
+
+
+
+        for (Integer integer : array) {
+            System.out.println(integer);
+        }
+
+
+        List<Integer> list1 = Arrays.asList(array);
+
+
+    }
+
+}
