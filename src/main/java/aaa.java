be8abf0aaf3706c30341f6f6a2bc1109f195c5cb
Your Name <you@example.com>
2019-07-02 14:02:48 +0800

    2:输出语句

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -2,5 +2,6 @@ public class aaa {
     public static void main(String[] args) {
         String s = new String("13456");
         System.out.println(s);
+        System.out.println("aaa");
     }
 }
