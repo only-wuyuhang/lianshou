d0abea8a418a39fc46660589bc465f13e4763e35
Your Name <you@example.com>
2019-07-02 14:08:21 +0800

    1:集合转数组

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -1,7 +1,21 @@
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class aaa {
     public static void main(String[] args) {
         String s = new String("13456");
         System.out.println(s);
         System.out.println("hahah");
+        ArrayList<String> list = new ArrayList<String>();
+        Collections.addAll(list,"asd","fgh","123","456");
+
+        String[] strings = list.toArray(new String[list.size()]);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
     }
 }
